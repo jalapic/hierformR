653be639b6daad105dba13221a607da1be10e7fe
James Curley <jc3181@columbia.edu>
2017-04-25 14:51:57 -0400

    updates

--- R/lastints.R
+++ R/lastints.R
@@ -4,13 +4,11 @@
 #' @return a list with each element being the last interaction for each pair of nodes
 #' up to that row of the dataframe
 #' @examples
-#' df<-data.frame(winner=c(1,2,3,1,2,3,2,1,2,3,3,1,2,3,4,3,1,3,2,1,1,1,1,2,2),
-#' loser=c(2,4,4,4,3,2,1,3,3,4,4,2,3,2,3,4,2,4,3,3,3,2,2,4,3)
-#' )
-#' lastints(df)
+#' lastints(ladybugs[,2:3])
 #' @export
 
 lastints<-function(df){
+  if(ncol(df)!=2) stop("input datframe does not have two columns")
   xx<- lapply(1:nrow(df), function(i) df[1:i,][!duplicated(df[1:i,], fromLast=T),])
   xx0 <- lapply(xx, function(x) apply(x,1,range))
   xx1 <- lapply(xx0, function(x) !duplicated(t(x), fromLast=T))
